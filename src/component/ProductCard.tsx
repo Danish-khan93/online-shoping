6efe32fd644c1be1a153820e088bf38a9fc3a999
Danish-khan93 <danishshoukat1994@gmail.com>
2024-04-21 23:02:53 +0500

    adding wish list icon in card

--- src/component/ProductCard.tsx
+++ src/component/ProductCard.tsx
@@ -1,25 +1,55 @@
-"use client"
+"use client";
 import React from "react";
 import { useState } from "react";
-import { Box, Typography, Rating } from "@mui/material";
+import { IoIosHeartEmpty } from "react-icons/io";
+import { LuEye } from "react-icons/lu";
+import { Box, Typography, Rating, IconButton } from "@mui/material";
 const ProductCard = () => {
   const [value, setValue] = useState<number | null>(2);
+
+
+// wishlist
+const addToWishList = () =>{
+  console.log("wish list item add");
+  
+}
+// wishlist
+
   return (
     <Box>
       <Box
-        className="w-[270px] h-[250px]"
+        className="w-[270px] h-[250px] flex justify-between p-[10px]"
         sx={{
           background: "url(https://picsum.photos/id/1/270/250)",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
         }}
-      ></Box>
+      >
+        <Box className="text-BG bg-Secondary2 rounded-md py-[5px] px-[8px] h-[30px]">
+          <Typography>discount</Typography>
+        </Box>
+        <Box className= 'flex flex-col gap-5'>
+          <Box className="bg-Secondary2 w-[30px] h-[30px] rounded-[50%] flex justify-center">
+            <IconButton onClick={addToWishList}>
+              <IoIosHeartEmpty className="text-BG" />
+            </IconButton>
+          </Box>
+          <Box className="bg-Secondary2 w-[30px] h-[30px] rounded-[50%] flex justify-center">
+            <IconButton>
+              <LuEye className="text-BG" />
+            </IconButton>
+          </Box>
+        </Box>
+      </Box>
       <Box className="py-[12px]">
-        <Typography className="text-semibold text-TEXT1 text-[16px]">Product name</Typography>
-        <Box className ="flex gap-8 my-[10px]">
-        <Typography className="text-Secondary2">new price</Typography>
-        <Typography className="text-TEXT3 line-through">-old price</Typography>
-
+        <Typography className="text-semibold text-TEXT1 text-[16px]">
+          Product name
+        </Typography>
+        <Box className="flex gap-8 my-[10px]">
+          <Typography className="text-Secondary2">new price</Typography>
+          <Typography className="text-TEXT3 line-through">
+            -old price
+          </Typography>
         </Box>
 
         <Rating
